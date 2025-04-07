e97c963f388b815ff953410bd0d7b3aa85eae4ba
jackbruce318 <128170835+jackbruce318@users.noreply.github.com>
2025-04-07 11:27:05 +0100

    Frontend Added
    Basic unlinked frontend implemented with view courses function working.

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,12 +3,24 @@ const db = new courseDAO();
 db.init()
 
 exports.home = function(req, res) {
-    res.send('<h1>Welcome to Dance Life!</h1>');
+    res.render("home", {
+        'title': 'Dance Life!',
+        'message': 'Welcome to the hub for Dance Life, a fun and inclusive club for people of all ages!'
+    })
 }
 
 exports.view_courses = function(req, res) {
-    res.send('<h1>Under Construction, come back later!</h1>');
-    db.getAllEntries();
+    //get all courses from the database and cast them to a list
+    db.getAllEntries()
+    .then((list) => {
+      res.render("viewCourses", { //render the entries template and send the courses data to the frontend
+        title: "Our Courses",
+        entries: list,
+      });
+    })
+    .catch((err) => {
+      console.log("promise rejected", err);
+    });
 }
 
 exports.course_details = function(req, res) {
